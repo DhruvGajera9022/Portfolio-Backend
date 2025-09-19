2809c4012de192e894136c601c86d896b0bef715
Dhruv Gajera <dhruvgajera05@gmail.com>
2025-09-19 11:27:15 +0530

    feature/auth - Add test setup and auth test file

---
+++ tests/testSetup.js
@@ -0,0 +1,16 @@
+const dotenv = require("dotenv");
+dotenv.config({ path: ".env.test" });
+
+const mongoose = require("mongoose");
+
+beforeAll(async () => {
+    await mongoose.connect(process.env.MONGO_URI_TEST, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
+});
+
+afterAll(async () => {
+    await mongoose.connection.dropDatabase();
+    await mongoose.connection.close();
+});
