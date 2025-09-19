e750969a0eafab695f7dd75ddff790ff2e4cfe6e
Dhruv Gajera <dhruvgajera05@gmail.com>
2025-09-19 11:58:29 +0530

    feature/auth - Add changes in ci.yml

--- tests/testSetup.js
+++ tests/testSetup.js
@@ -4,6 +4,7 @@ dotenv.config({ path: ".env.test" });
 const mongoose = require("mongoose");
 
 beforeAll(async () => {
+    jest.setTimeout(30000);
     await mongoose.connect(process.env.MONGO_URI_TEST, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
