82f365e342e8dc22f56f68f27d291279d61d9fdf
Dhruv Gajera <dhruvgajera05@gmail.com>
2025-09-20 09:30:33 +0530

    feature/profile - Add changes profile route

--- src/routes/profile.route.js
+++ src/routes/profile.route.js
@@ -1,17 +1,25 @@
+// routes/profile.routes.js
 const express = require("express");
 const router = express.Router();
 const authMiddleware = require("../middleware/auth.middleware");
 const profileController = require("../controllers/profile.controller");
+const validateRequest = require("../middleware/validation.middleware");
+const {
+    updateProfileSchema,
+} = require("../validations/profile.validation");
 
 /**
- * @route   POST /api/profile/me
+ * @route   GET /api/profile/me
  * @desc    Get Profile data
  * @access  Private
  */
-router.get(
-    "/me",
-    authMiddleware,
-    profileController.me
-);
+router.get("/", authMiddleware, profileController.me);
+
+/**
+ * @route   PUT /api/profile
+ * @desc    Update Profile data
+ * @access  Private
+ */
+router.put("/", authMiddleware, validateRequest(updateProfileSchema), profileController.updateProfile);
 
 module.exports = router;
